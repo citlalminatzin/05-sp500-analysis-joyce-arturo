78c55c00bab35662574c99e7289b7f30f4368333
artgora43 <arturo.gongora@ciencias.unam.mx>
2026-03-12 15:48:24 +0000

    fit data.py

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from yfinance import Ticker
+from pandas import read_csv
 
 def get_stock_data(ticker:str = "^GSPC", years: int = 5, path:str = "data/sp500.csv"):
     """Gets historic data for a ticker from yfinance API"""
@@ -10,7 +11,9 @@ def get_stock_data(ticker:str = "^GSPC", years: int = 5, path:str = "data/sp500.
 
 def read_data(path="data/pescados.csv"):
     """Lee los datos de un csv y te los devuelve"""
-    ...
+    file_path = 'sp500_2y.csv'
+    df = c
+
 
 def main():
     get_stock_data()
